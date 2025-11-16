61f87bad2bdb2b38cabd8a4e954930e78a98631a
Grace Ghansah <gracegh@stlghana.com>
2025-11-16 20:37:27 +0000

    updated

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -10,3 +10,27 @@ export interface ButtonProps {
   onClick?: () => void;
   type?: "button" | "submit" | "reset";
 }
+
+// interfaces/index.ts
+export interface Address {
+  state: string;
+  city: string;
+  country: string;
+}
+
+export interface Offers {
+  bed: string;
+  shower: string;
+  occupants: string;
+}
+
+export interface PropertyProps {
+  name: string;
+  address: Address;
+  rating: number;
+  category: string[];
+  price: number;
+  offers: Offers;
+  image: string;
+  discount?: string;
+}
